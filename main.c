1c1c3186b11aeb4bbf359c3810fb8eff6761ea43
hmanilow <hmanilow@student.21-school.ru>
2023-04-03 21:24:30 +0300

    liluzi

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+
+int next_id = 0, max = 0, id[65536];
+fd_set rdRead, rdWrite, active;
+char str[42 * 4096], rdBuf[42 * 4096], wrBuf[42 * 4097];
