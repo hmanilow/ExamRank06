732d8d6d19be2056f1e473bdb9ee518fb1dc5b09
Khussein Reber <73395161+hmanilow@users.noreply.github.com>
2023-04-03 21:54:13 +0300

    Update main.c

--- main.c
+++ main.c
@@ -8,3 +8,9 @@
 int next_id = 0, max = 0, id[65536];
 fd_set rdRead, rdWrite, active;
 char str[42 * 4096], rdBuf[42 * 4096], wrBuf[42 * 4097];
+
+void error(char *msg)
+{
+    write(2, msg, strlen(msg));
+    exit(1);
+}
